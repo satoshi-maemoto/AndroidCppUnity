f6cbf48d1ab4a545d198aedaa9ed151e17fce346
satoshi-maemoto <peugeot.106.s16@gmail.com>
2018-11-07 15:50:41 +0900

    add stringFromJNITest()

--- Cpp/app/src/androidTest/java/com/example/satoshi_maemoto/myapplication/ExampleInstrumentedTest.java
+++ Cpp/app/src/androidTest/java/com/example/satoshi_maemoto/myapplication/ExampleInstrumentedTest.java
@@ -2,8 +2,11 @@ package com.example.satoshi_maemoto.myapplication;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +19,50 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+    public native String stringFromJNI();
+
+    @Rule
+    public ActivityTestRule rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.satoshi_maemoto.myapplication", appContext.getPackageName());
+    }
+
+    private void showMessageOnUiThread(String message)
+    {
+        class ShowTextAction implements Runnable
+        {
+            private String message;
+            public ShowTextAction(String message)
+            {
+                this.message = message;
+            }
+
+            @Override
+            public void run() {
+                TextView messageText = rule.getActivity().findViewById(R.id.textMessage);
+                messageText.setText(this.message);
+            }
+        }
+
+        this.rule.getActivity().runOnUiThread(new ShowTextAction(message));
+    }
+
+    @Test
+    public void stringFromJNITest() throws InterruptedException {
+        String message = stringFromJNI();
+        this.showMessageOnUiThread(message);
+
+        Thread.sleep(5000);
+
+        assertEquals("Hello from C++ to InstrumentedTest", message);
     }
 }
--- Cpp/app/src/main/cpp/native-lib.cpp
+++ Cpp/app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,12 @@ Java_com_example_satoshi_1maemoto_myapplication_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_satoshi_1maemoto_myapplication_ExampleInstrumentedTest_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++ to InstrumentedTest";
+    return env->NewStringUTF(hello.c_str());
+}
